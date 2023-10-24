1f1a046546974ec99136758dee127a6a59a27154
Flor María <fmvaldevieso@utpl.edu.ec>
2023-10-23 21:47:52 -0500

    tercer commit, trabajando con Git y GitHubClasroom

---
+++ Proyecto2/src/proyecto2/Proyecto2.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package proyecto2;
+
+/**
+ *
+ * @author D E L L
+ */
+public class Proyecto2 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {System.out.println("Ejemplo dos basico de Java versionado a través de Git manejando desde GitHub además se usar GitHubClasroom");
+        // TODO code application logic here
+    }
+    
+}
